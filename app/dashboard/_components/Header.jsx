ca05b1676a4c3781dee548dd913ecc87b8330ca6
Likitha R <likhithar266@gmail.com>
2025-03-25 16:08:44 +0530

    feature: Added path to lists

--- app/dashboard/_components/Header.jsx
+++ app/dashboard/_components/Header.jsx
@@ -1,23 +1,30 @@
-
-
+"use client"
 import { UserButton } from '@clerk/nextjs';
 import Image from 'next/image'
 import { usePathname } from 'next/navigation';
-import React from 'react'
+import React, { useEffect } from 'react'
 
 
 function Header() {
 
     const path = usePathname();
+
+    useEffect(()=>{
+        console.log(path)
+    }, [])
     
     return (
     <div className='flex p-4 items-center justify-between bg-secondary shadow-sm'>
         <Image src={''} width ={160} height ={80} alt="logo"/>
 
         <ul className='flex gap-8'>
-            <li className='hover:text-primary hover:font-bold transition-all cursor-pointer'>Dashboard</li>
-            <li className='hover:text-primary hover:font-bold transition-all cursor-pointer'>Resume check</li>
-            <li className='hover:text-primary hover:font-bold transition-all cursor-pointer'>How it works?</li>
+            <li className={`hover:text-primary hover:font-bold transition-all cursor-pointer
+            ${path== '/dashboard && text-primary font-bold'}`}
+            >Dashboard</li>
+            <li className={`hover:text-primary hover:font-bold transition-all cursor-pointer
+            ${path== '/dashboard/resumecheck && text-primary font-bold'}`}>Resume check</li>
+            <li className={`hover:text-primary hover:font-bold transition-all cursor-pointer
+            ${path== '/dashboard/howitworks && text-primary font-bold'}`}>How it works?</li>
         </ul>
         <UserButton/>
     </div>
